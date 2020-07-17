5f69df28c749179673e03e7826f88bc9d92ad7d9
Duong Nguyen <ntd251@users.noreply.github.com>
2020-07-17 09:08:58 +0800

    Added Sider::HyperLogLog

---
+++ lib/sider/types/hyper_log_log.rb
@@ -0,0 +1,17 @@
+module Sider
+  class HyperLogLog
+    redis_methods %w[
+      pfadd
+      pfcount
+    ]
+
+    def pfmerge(destination, *other_keys)
+      redis_client.pfmerge(destination, key, *other_keys)
+    end
+
+    # Use `self.key` as destination
+    def pfmerge!(*other_keys)
+      redis_client.pfmerge(key, *other_keys)
+    end
+  end
+end
