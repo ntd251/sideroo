b7b285db2978016f11ab88ab059934f5cc9949ea
Duong Nguyen <ntd251@users.noreply.github.com>
2020-07-18 16:00:44 +0800

    Added first specs for string

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require "bundler/setup"
 require "sider"
+require "redis"
+require 'redis-namespace'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,4 +13,15 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.before(:all) do
+    Sider.configure do |c|
+      c.redis_client = Redis::Namespace.new('sider-specs', redis: Redis.new)
+    end
+  end
+
+  config.after(:each) do
+    Sider.redis_client.flushdb
+  end
 end
+
